4c0c37fd1650b63b16dc467e7322916346ff855f
KamilCetiner <cetiner.kamil84@gmail.com>
2021-01-10 00:22:52 +0100

    added urls and views

--- src/django_prj_1/settings.py
+++ src/django_prj_1/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'fscohort',
+    # 'fscohort.apps.FscohortCOnfig'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# PASSWORD_HASHERS = [
+    # 'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    # 'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    # 'django.contrib.auth.hashers.Argon2PasswordHasher',
+    # 'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+# ]
